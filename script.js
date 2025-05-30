f8808145d3af34f80b2b91ae56c3c518e8ca3447
lopatapolopate <tomasek.hebelka09@gmail.com>
2025-05-30 11:48:30 +0200

    .

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function odeslatFormular(event) {
     const zprava = document.getElementById('zprava').value;
     alert(`Děkujeme za zprávu, ${jmeno}!\nVaše zpráva: ${zprava}`);
 }
+// Lightbox otevření
+document.querySelectorAll('.image-clickable').forEach(img => {
+    img.addEventListener('click', function () {
+        document.getElementById('lightbox-img').src = this.src;
+        document.getElementById('lightbox-overlay').style.display = 'flex';
+    });
+});
+
+// Zavření lightboxu
+function zavritLightbox() {
+    document.getElementById('lightbox-overlay').style.display = 'none';
+}
